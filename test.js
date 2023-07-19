00e2f07a7fec196cf10b1e123ea157cb36d46859
Simone Scala <s.scala2005@gmail.com>
2023-07-19 11:22:12 +0200

    Wait for images to load

--- test.js
+++ test.js
@@ -14,7 +14,39 @@ window.addEventListener("resize", resizeGameCanvas, false);
 
 
 // ----- IMAGES ----- //
+let imgURLs = [
+    "https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3017&q=80",
+    "https://as2.ftcdn.net/v2/jpg/05/25/08/09/1000_F_525080936_JEpnKXh2siYKBPpsqd98pbbcIzy4ySKz.jpg",
+    "brick.png"
+];
+let imgAliases = [
+    "img1",
+    "img2",
+    "brick"
+];
+let loadedImages = 0;
 let images = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    function imgLoaded(url) {
+        loadedImages++;
+        console.log("fatto:", loadedImages, url);
+
+        if(loadedImages == imgURLs.length){
+            document.getElementById("loading").remove();
+            document.getElementById("game").style.display = "block";
+            window.requestAnimationFrame(gameLoop);
+        }
+    }
+    
+    for(let i = 0; i < imgURLs.length; i++) {
+        images[imgAliases[i]] = new Image();
+        images[imgAliases[i]].src = imgURLs[i];
+        images[imgAliases[i]].onload = function() {
+            imgLoaded(imgURLs[i]);
+        }
+    }
+});
 // ----- IMAGES ----- //
 
 
@@ -24,14 +56,12 @@ function gameLoop(timeStamp) {
     prevTime = timeStamp;
 
     fps = Math.round(1000 / deltaTime);
-    console.log(fps + " fps");
 
     update();
     draw();
 
     window.requestAnimationFrame(gameLoop);
 }
-window.requestAnimationFrame(gameLoop);
 
 
 function resizeGameCanvas() {
@@ -55,12 +85,16 @@ function update() {
 
 
 function draw() {
-    game.fillStyle = "black";
+    game.fillStyle = "rgb(200, 250, 250)";
     game.fillRect(0, 0, gWIDTH, gHEIGHT);
 
-    game.fillStyle = "white";
+    game.fillStyle = "black";
     game.font = "24px courier";
     game.textAlign = "start";
     game.textBaseline = "top";
-    game.fillText("FPS: " + fps, 15, 10);
+    game.fillText(gWIDTH + " x " + gHEIGHT + " / FPS: " + fps, 15, 10);
+
+    game.drawImage(images["img1"], 100, 100);
+    game.drawImage(images["img2"], 250, 250);
+    game.drawImage(images["brick"], 400, 400);
 }
