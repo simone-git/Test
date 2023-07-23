5196ae9a11f589b736c60bfe00106b46b0935868
Simone <s.scala2005@gmail.com>
2023-07-23 21:07:55 +0200

    Added levels buttons, without numbers

--- main.js
+++ main.js
@@ -61,53 +61,53 @@ let imgAliases = [
     "levels_padlock_over"
 ];
 let images = [];  // Container of only images
-
+let d = 13;
 let boxSpecs = [  // Syntax: boxAlias (id), imgAlias (source1), imgAlias_over (source2), specs: (CORNERS: left, top, right, bottom; CENTER: x, y, w / h); Values: W[0, 160] / H[0, 90]
     // Common
     ["common_bg",               "common_bg",            "common_bg",                "CORNERS",  0,      0,      160,    90],
 
     // Home
-    ["home_title",              "home_title",           "home_title",               "CENTER",   80,     18,     "W",    80],
+    ["home_title",              "home_title",           "home_title",               "CENTER",   80,     18,     "H",    30],
     ["home_play",               "home_play",            "home_play_over",           "CENTER",   80,     45,     "W",    32],
     ["home_set",                "home_set",             "home_set_over",            "CENTER",   32,     72,     "W",    32],
     ["home_inv",                "home_inv",             "home_inv_over",            "CENTER",   80,     63,     "W",    32],
 
     // Levels
-    ["levels_title",            "levels_title",         "levels_title",             "CENTER",   80,     15,     "W",    64],
+    ["levels_title",            "levels_title",         "levels_title",             "CENTER",   80,     11,     "H",    20],
     ["levels_back",             "levels_back",          "levels_back_over",         "CENTER",   16,     81,     "W",    24],
-    // #region Levels 1x1 to 4x4
-    // ["levels_level_1",          "levels_level",         "levels_level_over",        "CENTER",   0.2,    0.3,    "W",    0.1],
-    // ["levels_padlock_1",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.2,    0.3,    "W",    0.05],
-    // ["levels_level_2",          "levels_level",         "levels_level_over",        "CENTER",   0.8,    0.3,    "W",    0.1],
-    // ["levels_padlock_2",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.8,    0.3,    "W",    0.05],
-    // ["levels_level_3",          "levels_level",         "levels_level_over",        "CENTER",   0.2,    0.9,    "W",    0.1],
-    // ["levels_padlock_3",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.2,    0.9,    "W",    0.05],
-    // ["levels_level_4",          "levels_level",         "levels_level_over",        "CENTER",   0.8,    0.9,    "W",    0.1],
-    // ["levels_padlock_4",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.8,    0.9,    "W",    0.05],
-    // ["levels_level_5",          "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_5",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_6",          "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_6",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_7",          "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_7",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_8",          "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_8",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_9",          "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_9",        "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_10",         "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_10",       "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_11",         "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_11",       "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_12",         "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_12",       "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_13",         "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_13",       "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_14",         "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_14",       "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_15",         "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_15",       "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05],
-    // ["levels_level_16",         "levels_level",         "levels_level_over",        "CENTER",   0.1,    0.9,    "W",    0.1],
-    // ["levels_padlock_16",       "levels_padlock",       "levels_padlock_over",      "CENTER",   0.1,    0.9,    "W",    0.05]
+    // #region Levels 4x4
+    ["levels_level_1",          "levels_level",         "levels_level_over",        "CENTER",   53,     27,     "W",    d],
+    ["levels_padlock_1",        "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     27,     "W",    d],
+    ["levels_level_2",          "levels_level",         "levels_level_over",        "CENTER",   71,     27,     "W",    d],
+    ["levels_padlock_2",        "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     27,     "W",    d],
+    ["levels_level_3",          "levels_level",         "levels_level_over",        "CENTER",   89,     27,     "W",    d],
+    ["levels_padlock_3",        "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     27,     "W",    d],
+    ["levels_level_4",          "levels_level",         "levels_level_over",        "CENTER",   107,    27,     "W",    d],
+    ["levels_padlock_4",        "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    27,     "W",    d],
+    ["levels_level_5",          "levels_level",         "levels_level_over",        "CENTER",   53,     45,     "W",    d],
+    ["levels_padlock_5",        "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     45,     "W",    d],
+    ["levels_level_6",          "levels_level",         "levels_level_over",        "CENTER",   71,     45,     "W",    d],
+    ["levels_padlock_6",        "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     45,     "W",    d],
+    ["levels_level_7",          "levels_level",         "levels_level_over",        "CENTER",   89,     45,     "W",    d],
+    ["levels_padlock_7",        "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     45,     "W",    d],
+    ["levels_level_8",          "levels_level",         "levels_level_over",        "CENTER",   107,    45,     "W",    d],
+    ["levels_padlock_8",        "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    45,     "W",    d],
+    ["levels_level_9",          "levels_level",         "levels_level_over",        "CENTER",   53,     63,     "W",    d],
+    ["levels_padlock_9",        "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     63,     "W",    d],
+    ["levels_level_10",         "levels_level",         "levels_level_over",        "CENTER",   71,     63,     "W",    d],
+    ["levels_padlock_10",       "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     63,     "W",    d],
+    ["levels_level_11",         "levels_level",         "levels_level_over",        "CENTER",   89,     63,     "W",    d],
+    ["levels_padlock_11",       "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     63,     "W",    d],
+    ["levels_level_12",         "levels_level",         "levels_level_over",        "CENTER",   107,    63,     "W",    d],
+    ["levels_padlock_12",       "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    63,     "W",    d],
+    ["levels_level_13",         "levels_level",         "levels_level_over",        "CENTER",   53,     81,     "W",    d],
+    ["levels_padlock_13",       "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     81,     "W",    d],
+    ["levels_level_14",         "levels_level",         "levels_level_over",        "CENTER",   71,     81,     "W",    d],
+    ["levels_padlock_14",       "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     81,     "W",    d],
+    ["levels_level_15",         "levels_level",         "levels_level_over",        "CENTER",   89,     81,     "W",    d],
+    ["levels_padlock_15",       "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     81,     "W",    d],
+    ["levels_level_16",         "levels_level",         "levels_level_over",        "CENTER",   107,    81,     "W",    d],
+    ["levels_padlock_16",       "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    81,     "W",    d]
     // #endregion
 ];
 let boxes = [];  // Container of boxes with refs to images
@@ -300,10 +300,10 @@ function draw() {
             drawBox("common_bg");
             drawBox("levels_title");
             drawBox("levels_back");
-            // drawBox("levels_level_1");
-            // drawBox("levels_level_2");
-            // drawBox("levels_level_3");
-            // drawBox("levels_level_4");
+            for(let i = 1; i <= 16; i++) {
+                drawBox("levels_level_" + i);
+                drawBox("levels_padlock_" + i);
+            }
             break;
     }
 
@@ -313,6 +313,6 @@ function draw() {
     game.textBaseline = "top";
     let text = "FPS: " + fps + " / " +
         gWIDTH + " x " + gHEIGHT + " / " + 
-        "MOUSE: (" + mouseInfo["x"] + ", " + mouseInfo["y"] + ")";
+        "MOUSE: (" + (mouseInfo["x"] / gWIDTH * 160) + ", " + (mouseInfo["y"] / gHEIGHT * 90) + ")";
     game.fillText(text, 8, 4);
 }
