267b4c5639fc477e2d1423a1aff33afdf83cbb52
Simone Scala <s.scala2005@gmail.com>
2023-08-03 22:21:13 +0200

    Pre prova con classi

--- main.js
+++ main.js
@@ -7,119 +7,43 @@ sceneInfo["scene"] = "home";
 
 let prevTime = 0, fps = 0;
 
-/*let frameCanvas = document.createElement("canvas");  // Can also use OffscreenCanvas
-frameCanvas.setAttribute("id", "frame");
-let frame = frameCanvas.getContext("2d");*/
+// let frameCanvas = document.createElement("canvas");  // Can also use OffscreenCanvas
+// frameCanvas.setAttribute("id", "frame");
+// let frame = frameCanvas.getContext("2d");
 
 let gWIDTH, gHEIGHT, ratio = 16 / 9;
 
 resizeCanvas(false);
 window.addEventListener("resize", resizeCanvas, false);
+
+
+function resizeCanvas(boxImgs = true) {
+    if(window.innerWidth / window.innerHeight >= ratio) {
+        gWIDTH = Math.round(window.innerHeight * ratio);
+        gHEIGHT = window.innerHeight;
+        canvas.width = gWIDTH;
+        canvas.height = gHEIGHT;
+    } else {
+        gWIDTH = window.innerWidth;
+        gHEIGHT = Math.round(window.innerWidth / ratio);
+        canvas.width = gWIDTH;
+        canvas.height = gHEIGHT;
+    }
+
+    if(boxImgs) {
+        boxImages();
+    }
+}
 // ----- CANVAS ----- //
 
 
 // ----- LEVELS ----- //
 let levels = [];
 levels[0] = { "": [] };
+// ----- LEVELS ----- //
 
 
 // ----- IMAGES AND BOXES ----- //
-let imgPaths = [
-    // Common
-    "data/images/common/background.png",
-
-    // Home
-    "data/images/home/title.png",
-    "data/images/home/play.png",
-    "data/images/home/play-over.png",
-    "data/images/home/settings.png",
-    "data/images/home/settings-over.png",
-    "data/images/home/inventory.png",
-    "data/images/home/inventory-over.png",
-
-    // Levels
-    "data/images/levels/title.png",
-    "data/images/levels/back.png",
-    "data/images/levels/back-over.png",
-    "data/images/levels/level.png",
-    "data/images/levels/level-over.png",
-    "data/images/levels/padlock.png",
-    "data/images/levels/padlock-over.png"
-];
-let imgAliases = [
-    //Common
-    "common_bg",
-
-    // Home
-    "home_title",
-    "home_play",
-    "home_play_over",
-    "home_set",
-    "home_set_over",
-    "home_inv",
-    "home_inv_over",
-
-    // Levels
-    "levels_title",
-    "levels_back",
-    "levels_back_over",
-    "levels_level",
-    "levels_level_over",
-    "levels_padlock",
-    "levels_padlock_over"
-];
-let images = [];  // Container of only images
-let d = 13;
-let boxSpecs = [  // Syntax: boxAlias (id), imgAlias (source1), imgAlias_over (source2), specs: (CORNERS: left, top, right, bottom; CENTER: x, y, w / h); Values: W[0, 160] / H[0, 90]
-    // Common
-    ["common_bg",               "common_bg",            "common_bg",                "CORNERS",  0,      0,      160,    90],
-
-    // Home
-    ["home_title",              "home_title",           "home_title",               "CENTER",   80,     18,     "H",    30],
-    ["home_play",               "home_play",            "home_play_over",           "CENTER",   80,     45,     "W",    32],
-    ["home_set",                "home_set",             "home_set_over",            "CENTER",   32,     72,     "W",    32],
-    ["home_inv",                "home_inv",             "home_inv_over",            "CENTER",   80,     63,     "W",    32],
-
-    // Levels
-    ["levels_title",            "levels_title",         "levels_title",             "CENTER",   80,     11,     "H",    20],
-    ["levels_back",             "levels_back",          "levels_back_over",         "CENTER",   16,     81,     "W",    24],
-    // #region Levels 4x4
-    ["levels_level_1",          "levels_level",         "levels_level_over",        "CENTER",   53,     27,     "W",    d],
-    ["levels_padlock_1",        "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     27,     "W",    d],
-    ["levels_level_2",          "levels_level",         "levels_level_over",        "CENTER",   71,     27,     "W",    d],
-    ["levels_padlock_2",        "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     27,     "W",    d],
-    ["levels_level_3",          "levels_level",         "levels_level_over",        "CENTER",   89,     27,     "W",    d],
-    ["levels_padlock_3",        "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     27,     "W",    d],
-    ["levels_level_4",          "levels_level",         "levels_level_over",        "CENTER",   107,    27,     "W",    d],
-    ["levels_padlock_4",        "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    27,     "W",    d],
-    ["levels_level_5",          "levels_level",         "levels_level_over",        "CENTER",   53,     45,     "W",    d],
-    ["levels_padlock_5",        "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     45,     "W",    d],
-    ["levels_level_6",          "levels_level",         "levels_level_over",        "CENTER",   71,     45,     "W",    d],
-    ["levels_padlock_6",        "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     45,     "W",    d],
-    ["levels_level_7",          "levels_level",         "levels_level_over",        "CENTER",   89,     45,     "W",    d],
-    ["levels_padlock_7",        "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     45,     "W",    d],
-    ["levels_level_8",          "levels_level",         "levels_level_over",        "CENTER",   107,    45,     "W",    d],
-    ["levels_padlock_8",        "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    45,     "W",    d],
-    ["levels_level_9",          "levels_level",         "levels_level_over",        "CENTER",   53,     63,     "W",    d],
-    ["levels_padlock_9",        "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     63,     "W",    d],
-    ["levels_level_10",         "levels_level",         "levels_level_over",        "CENTER",   71,     63,     "W",    d],
-    ["levels_padlock_10",       "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     63,     "W",    d],
-    ["levels_level_11",         "levels_level",         "levels_level_over",        "CENTER",   89,     63,     "W",    d],
-    ["levels_padlock_11",       "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     63,     "W",    d],
-    ["levels_level_12",         "levels_level",         "levels_level_over",        "CENTER",   107,    63,     "W",    d],
-    ["levels_padlock_12",       "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    63,     "W",    d],
-    ["levels_level_13",         "levels_level",         "levels_level_over",        "CENTER",   53,     81,     "W",    d],
-    ["levels_padlock_13",       "levels_padlock",       "levels_padlock_over",      "CENTER",   53,     81,     "W",    d],
-    ["levels_level_14",         "levels_level",         "levels_level_over",        "CENTER",   71,     81,     "W",    d],
-    ["levels_padlock_14",       "levels_padlock",       "levels_padlock_over",      "CENTER",   71,     81,     "W",    d],
-    ["levels_level_15",         "levels_level",         "levels_level_over",        "CENTER",   89,     81,     "W",    d],
-    ["levels_padlock_15",       "levels_padlock",       "levels_padlock_over",      "CENTER",   89,     81,     "W",    d],
-    ["levels_level_16",         "levels_level",         "levels_level_over",        "CENTER",   107,    81,     "W",    d],
-    ["levels_padlock_16",       "levels_padlock",       "levels_padlock_over",      "CENTER",   107,    81,     "W",    d]
-    // #endregion
-];
-let boxes = [];  // Container of boxes with refs to images
-
 let loadedImages = 0;
 document.addEventListener("DOMContentLoaded", function() {
     for(let i = 0; i < imgPaths.length; i++) {
@@ -179,12 +103,6 @@ function boxImages() {
 }
 // ----- IMAGES AND BOXES ----- //
 
-// ----- FONTS ----- //
-let fonts = [];
-
-//fonts["levels_level"] = new FontFace("Library", "data/fonts/Library 3 am");
-// ----- FONTS ----- //
-
 
 // ----- MOUSE ----- //
 let mouseInfo = [];
@@ -218,6 +136,14 @@ function mouseEvent(event) {
 document.addEventListener("mousemove", mouseEvent);
 document.addEventListener("mousedown", mouseEvent);
 // document.addEventListener("mouseup", mouseEvent);
+
+
+function mouseTestBox(box) {
+    let x = mouseInfo["x"];
+    let y = mouseInfo["y"];
+
+    return boxes[box] != undefined && x >= boxes[box].left && x <= boxes[box].right && y >= boxes[box].top && y <= boxes[box].bottom;
+}
 // ----- MOUSE ----- //
 
 
@@ -243,33 +169,6 @@ function gameLoop(timeStamp) {
     draw();
 
     window.requestAnimationFrame(gameLoop);
-}
-
-
-function resizeCanvas(boxImgs = true) {
-    if(window.innerWidth / window.innerHeight >= ratio) {
-        gWIDTH = Math.round(window.innerHeight * ratio);
-        gHEIGHT = window.innerHeight;
-        canvas.width = gWIDTH;
-        canvas.height = gHEIGHT;
-    } else {
-        gWIDTH = window.innerWidth;
-        gHEIGHT = Math.round(window.innerWidth / ratio);
-        canvas.width = gWIDTH;
-        canvas.height = gHEIGHT;
-    }
-
-    if(boxImgs) {
-        boxImages();
-    }
-}
-
-
-function mouseTestBox(box) {
-    let x = mouseInfo["x"];
-    let y = mouseInfo["y"];
-
-    return boxes[box] != undefined && x >= boxes[box].left && x <= boxes[box].right && y >= boxes[box].top && y <= boxes[box].bottom;
 }
 
 // TODO: Fully manage keyboard inputs and add keyboard menu navigation
